a58edc6dbb363ada25abf55e85d1b4e650ae2e80
Teddystonecold08 <ernestlrty@hotmail.com>
2023-03-30 15:13:51 +0000

    create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# creating file in /tmp
+# Create a file in /tmp
 
-file {'/tmp/school':
-ensure => 'file',
-owner => 'www-data',
-group => 'www-data',
-mode => '0744',
-content => 'I love Puppet',
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
