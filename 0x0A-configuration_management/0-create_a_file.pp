3a52273412ead5d2b47352201a1ec97cf7ead389
Teddystone08 <ernestlrty@hotmail.com>
2024-08-31 22:02:19 +0000

    update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # create file in tmp
-file {'tmp/school':
-owner => www-data,
-mode => '0744',
-group => www-data,
+file {
+owner 	=> www-data,
+permission => '0744',
+group 	=> www-data,
 content => 'I love Puppet'
 }
